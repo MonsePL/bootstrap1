2b6ba99bccb519e16c6459834a93d52cf2c7bdfa
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-05-16 23:35:00 -0500

    ejercicio de practica primero con boot

---
+++ js/panda.js
@@ -0,0 +1,10 @@
+var imgTotales = document.getElementsByClassName('img');
+var eliminarElem = document.getElementsByClassName('quitar');
+
+
+for (var i = 0; i < eliminarElem.length; i++) {
+  eliminarElem[i].addEventListener("click",ocultarImagen);}
+
+  function ocultarImagen() {
+    this.parentNode.style.visibility = "none";
+  }
